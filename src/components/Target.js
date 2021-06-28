a4fb14a7d47cf23fcfb0cd38301a25ff07c116d2
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-06-28 17:48:12 +0100

    Target styling

--- src/components/Target.js
+++ src/components/Target.js
@@ -1,13 +1,15 @@
+import '../styles/Target.css'
+
 const Target = ({ clientPos, getChoice, characters }) => {
     const positionTargetX = clientPos[0] + clientPos[2].left - 20
     const positionTargetY = clientPos[1] + clientPos[2].top - 20
     return (
-        <div style={{ border: '2px  solid red', width: '40px', height: '40px', position: "fixed", top: `${positionTargetY}px`, left: `${positionTargetX}px` }}>
-            <div style={{ display: 'grid', position: "fixed", top: `${positionTargetY + 43}px`, left: `${positionTargetX - 10}px` }}>
+        <div id="targetBox" style={{ top: `${positionTargetY}px`, left: `${positionTargetX}px` }}>
+            <div style={{ display: 'grid', position: "fixed", top: `${positionTargetY + 47}px`, left: `${positionTargetX - 10}px` }}>
                 {characters.map((char) => {
                     return (
                         <div key={char.name}>
-                            <button onClick={getChoice}>{char.name}</button>
+                            <button style={{ width: '100%' }} onClick={getChoice}>{char.name}</button>
                         </div>
                     )
                 })}
